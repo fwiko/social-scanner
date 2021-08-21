47a6d50cf8e5183ced249a85bec857896e38a5c2
BuildTools <raffsimms23@gmail.com>
2021-08-21 19:11:52 +0100

    Changed request type and Added prettier logging
    Changed the request that is made from POST to GET
    Made log output of requests look better
    Added JSON support with ?json=1

--- routes/routing.js
+++ routes/routing.js
@@ -3,7 +3,7 @@ const socialChecker = require('../modules/medium_checker');
 const mediumChecker = require('../modules/medium_checker')
 const fs = require('fs');
  
-async function checkUsernames(username, callback) {
+function checkUsernames(username, callback) {
     const socialChecker = new mediumChecker(username);
     socialChecker.checkAll((mediums) => {
         callback(mediums);
@@ -15,51 +15,66 @@ function getUsernameCount(callback) {
         if (err) throw err;
         let information = JSON.parse(data);
         const username_count = information.usernames_checked;
-        console.log(username_count);
         return callback(username_count);
     })
 }
 
-// TODO - username alpha, number, underscore and fullstop validation
-
-router.post('/', async (req, res) => {
-    const { username } = req.body;
-    if (username != undefined && username != "" && username.length <= 30) {
-        var ip = req.headers['cf-connecting-ip'] || req.headers['x-forwarded-for'] || req.ip;
-        await checkUsernames(username, (mediums) => {
-            console.log(`> ${ip} checked ${username}`)
-            fs.readFile('./data/information.json', (err, data) => {
-                if (err) throw err;
-                let information = JSON.parse(data);
-                information.usernames_checked += 1;
-                const username_count = information.usernames_checked;
-                const info = JSON.stringify(information);
-                fs.writeFileSync('./data/information.json', info);
-                res.render('index', {
-                    mediums: mediums,
-                    username,
-                    usernamesChecked: username_count
-                });
-            });
-
-        });     
-    } else {
-        getUsernameCount((usernames_checked)=>{
-            console.log(usernames_checked)
-            res.render('index', {
-                message: "Invalid username input",
-                usernamesChecked: usernames_checked
-            });
-        })
-    }
-});
-
 router.get('/', (req, res) => {
     getUsernameCount((usernames_checked)=>{
-        console.log(usernames_checked)
-        res.render('index', {
-            usernamesChecked: usernames_checked
-        });
+        
+        if ("username" in req.query) {
+            const { username, json } = req.query;
+            if (username != undefined && username != "" && username.length <= 30) {
+                var ip = req.headers['cf-connecting-ip'] || req.headers['x-forwarded-for'] || req.ip;
+
+                // Checking usernames availability
+                checkUsernames(username, (mediums) => {
+
+                    // Request logging
+                    console.log(`\n┌ ${ip} checked "${username}"\n├───────────────────⎯⎯⎯⎯⎯⎯⎯─────⎯⎯⎯⎯⎯`);
+                    mediums.forEach(element => {
+                        console.log(`│ ${element.service_name}${" ".repeat(((Math.max.apply(Math, mediums.map((e)=>{
+                            return e.service_name.length
+                        })))-element.service_name.length)+14)} ${element.availability ? "  Available" : "Unavailable"}`)
+                    });
+                    
+                    // Updating usernames checked statistics
+                    fs.readFile('./data/information.json', (err, data) => {
+                        if (err) throw err;
+                        let information = JSON.parse(data);
+                        information.usernames_checked += 1;
+                        const username_count = information.usernames_checked;
+                        const info = JSON.stringify(information);
+                        fs.writeFileSync('./data/information.json', info);
+
+                        // Returning results from request
+                        if (json==1) {
+                            return res.json({
+                                username: username,
+                                results: mediums
+                            })
+                        }
+                        return res.render('index', {
+                            mediums: mediums,
+                            username,
+                            usernamesChecked: username_count
+                        });
+                    });
+                });
+
+            } else {
+                return res.render('index', {
+                    message: "Invalid username input",
+                    usernamesChecked: usernames_checked
+                });
+            }
+        } else {
+
+            return res.render('index', {
+                usernamesChecked: usernames_checked
+            });
+
+        }
     })
 });
 
